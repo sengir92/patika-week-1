b729cba36f28cc2d9a3f6f74ceb220f3f26128e0
sengir92 <onursengir92@gmail.com>
2023-12-19 01:54:40 +0300

    AmstrongSayi

---
+++ AmstrongSayi/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number: ");
+        int number = scanner.nextInt();
+
+        int i = 0,sum=0;
+
+        while(number != 0)
+        {
+            sum=(number%10)+sum;
+            number /= 10;
+            ++i;
+        }
+        System.out.println(sum);
+
+    }
+}
